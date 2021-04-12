049896d0b7594e75ddbeda73e6b1e94a059e6a93
NikKokor <62161082+NikKokor@users.noreply.github.com>
2021-04-12 16:18:40 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const _ = require('colorette');
+
+console.log(_.green(_.bold('Hello, world!')));
+console.log(_.blue(_.strikethrough(_.bold('Hello, world!'))));
+console.log(_.bgCyanBright('Hello, world!'));
+console.log(_.italic('Hello, world!'));
+console.log(_.dim('Hello, world!'));
